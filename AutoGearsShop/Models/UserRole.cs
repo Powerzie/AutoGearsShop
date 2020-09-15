5cfa6efb4d4f8b64845084f06b6a3a90d181d4c4
Gera_chf9@student.itstep.org <Gera_chf9@student.itstep.org>
2020-09-15 11:50:30 +0300

    Added Models

---
+++ AutoGearsShop/Models/UserRole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoGearsShop.Models
+{
+    public class UserRole
+    {
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "RoleName")]
+        [Required(ErrorMessage = "Missing Role Name")]
+        public string RoleName { get; set; }
+    }
+}
---
+++ AutoGearsShop/Models/UserVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoGearsShop.Models
+{
+    public class UserVM
+    {
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "RoleName")]
+        [Required(ErrorMessage = "Missing User Name")]
+        public string UserName { get; set; }
+
+    }
+}
